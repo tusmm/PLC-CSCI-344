4040e97e2e98e0774103ad45181a4e25102d69bc
Ryan Ong <tusm@localhost.localdomain>
2024-02-24 11:23:24 -0500

    Implemented FunctionCallNode and somewhat of ParamNode

--- src/nodes/FunctionCallNode.java
+++ src/nodes/FunctionCallNode.java
@@ -1,5 +1,58 @@
 package nodes;
 
-public class FunctionCallNode {
-    
+import java.net.IDN;
+import java.util.ArrayList;
+
+import provided.Token;
+import provided.TokenType;
+
+public class FunctionCallNode extends OperandNode {
+    IDNode id;
+    ParamNode params;
+
+    public FunctionCallNode(IDNode id, ParamNode params) {
+        this.id = id;
+        this.params = params;
+    }
+
+    public static FunctionCallNode parseFunctionCallNode(ArrayList<Token> tokens) {
+        if (isEmptyTokensList(tokens)) {
+            System.out.println("Handle exception here");
+        }
+        if (tokens.get(0).getTokenType() != TokenType.FC_HEADER) {
+            System.out.println("Handle error");
+        }
+        
+        tokens.remove(0); // dequeue the FC_Header
+        IDNode id = IDNode.parseIDNode(tokens);
+        
+        if (isEmptyTokensList(tokens)) {
+            System.out.println("Handle exception here");
+        } 
+        if (tokens.get(0).getTokenType() != TokenType.L_BRACKET) {
+            System.out.println("Handle error");
+        }
+
+        // do you need to pop here? wont the loewr level method call do it?
+        ParamNode pm = ParamNode.parseParamNode(tokens);
+        
+        if (isEmptyTokensList(tokens)) {
+            System.out.println("Handle exception here");
+        } 
+        if (tokens.get(0).getTokenType() != TokenType.R_BRACKET) {
+            System.out.println("Handle error");
+        }
+
+       // do you need to pop here? wont lower leve method call do it?
+       return new FunctionCallNode(id, pm); 
+    }
+
+    @Override
+    public String convertToJott() {
+        return "::" + id.convertToJott() + "[" + params.convertToJott() + "]";
+    }
+
+    private static boolean isEmptyTokensList(ArrayList<Token> tokens) {
+        return tokens.size() != 0;
+    }
 }
---
+++ src/nodes/ParamNode.java
@@ -0,0 +1,19 @@
+package nodes;
+
+import java.util.ArrayList;
+
+import provided.Token;
+
+public class ParamNode extends OperandNode {
+
+    public static ParamNode parseParamNode(ArrayList<Token> tokens) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'parseParamNode'");
+    }
+
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+    }
+    
+}
