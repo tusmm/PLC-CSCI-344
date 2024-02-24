138f223ee187ba828ee009e5d4b0650175c4be9e
Ryan Ong <tusm@localhost.localdomain>
2024-02-24 10:56:06 -0500

    Implemented NumberNode from activity

--- src/nodes/NumberNode.java
+++ src/nodes/NumberNode.java
@@ -1,5 +1,29 @@
 package nodes;
 
-public class NumberNode {
-    
+import java.util.ArrayList;
+
+import provided.Token;
+import provided.TokenType;
+
+
+public class NumberNode extends OperandNode {
+   Token token;
+   
+   public NumberNode(Token token) { 
+        this.token = token;
+   }
+
+   public static NumberNode parseNumberNode(ArrayList<Token> tokens) {
+        // first check if the token list is empty
+        if (tokens.size() == 0) {
+            System.out.println("Handle exception here");
+        }        
+        Token name = tokens.get(0); // get the front of the token
+        if (name.getTokenType() == TokenType.NUMBER) {
+            tokens.remove(0); // take off the first element
+            return new NumberNode(name);
+        }
+        System.out.println("Handle exception here");
+        return null;
+   }
 }
