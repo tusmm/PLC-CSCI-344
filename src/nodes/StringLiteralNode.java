d36f2fc3c8cd15980a8c27b1f77c5ab558189a3a
Lucy Hu <lh5844rit@gmail.com>
2024-03-06 17:16:34 -0500

    completed string literal node

---
+++ src/nodes/StringLiteralNode.java
@@ -0,0 +1,64 @@
+package nodes;
+
+import java.util.ArrayList;
+
+import provided.Token;
+import provided.TokenType;
+
+public class StringLiteralNode implements ExpressionNode{
+    Token token;
+
+    public StringLiteralNode(Token token) {
+        this.token = token;
+    }
+
+    public static StringLiteralNode parseStringLiteralNode(ArrayList<Token> tokens) {
+        // check if empty
+        if (tokens.size() == 0) {
+            // handle error 
+            // missing string literal
+            return null;
+        }
+
+        Token token = tokens.get(0);
+        if (token.getTokenType() != TokenType.STRING) {
+            System.err.println("Syntax Error:");
+            String unexpected = token.getTokenType().toString().toLowerCase();
+            System.err.println("Expected string but got " + unexpected);
+            System.err.println(token.getFilename() + ":" + token.getLineNum());
+            return null;
+        }
+
+        return new StringLiteralNode(token);
+    }
+    
+    @Override
+    public String convertToJott() {
+        return token.getToken();
+    }
+
+    @Override
+    public String convertToJava(String className) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJava'");
+    }
+
+    @Override
+    public String convertToC() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToC'");
+    }
+
+    @Override
+    public String convertToPython() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToPython'");
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+    
+}
